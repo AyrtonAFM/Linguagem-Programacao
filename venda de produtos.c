73c00d80ca8b05560333c02b266df0019ca1f9f9
AyrtonAFM <115302284+AyrtonAFM@users.noreply.github.com>
2023-04-01 21:17:14 -0300

    Update venda de produtos.c

--- venda de produtos.c
+++ venda de produtos.c
@@ -30,11 +30,68 @@ int main()
         printf ( "4 parcelas com 4,2%% de juros\n");
         printf ( "5 parcelas com 5,9%% de juros\n");
 
-        printf ("\nQuantas parcelas você quer? \n");
+        printf ("\nQuantas parcelas voce quer? \n");
         scanf ("%d",&quantidadeDeParcelas);
 
-        printf("Saida: %f",valorDaCompra);
+        if (quantidadeDeParcelas <=2 & quantidadeDeParcelas>1) {
+
+            juros = 1.1/100;
+            valorTotalDaCompra = valorDaCompra * (1 + juros);
+            parcelas = valorTotalDaCompra/2;
+            printf("\nPerfeito! O valor total da compra sera de R$ %.2f",valorTotalDaCompra);
+            printf ("\nCom 2 parcelas de R$ %.2f",parcelas,"\n");
+            printf ("\nCOMPRA REALIZADA COM SUCESSO!!");
+            printf ("\nOBRIGADO PELA PREFERENCIA!!\nVOLTE SEMPRE!!\n\n");
+
+		}else if (quantidadeDeParcelas <=3 & quantidadeDeParcelas>2) {
+
+            juros = 2.7/100;
+            valorTotalDaCompra = valorDaCompra * (1 + juros);
+            parcelas = valorTotalDaCompra/3;
+            printf("\nPerfeito! O valor total da compra sera de R$ %.2f",valorTotalDaCompra);
+            printf("\nCom 3 parcelas de R$ %.2f",parcelas,"\n");
+            printf("\nCOMPRA REALIZADA COM SUCESSO!!");
+            printf("\nOBRIGADO PELA PREFERENCIA!!\nVOLTE SEMPRE!!\n\n");
+
+		}else if (quantidadeDeParcelas <=4 & quantidadeDeParcelas>3) {
+
+            juros = 4.2/100;
+            valorTotalDaCompra = valorDaCompra * (1 + juros);
+            parcelas = valorTotalDaCompra/4;
+            printf("\nPerfeito! O valor total da compra sera de R$ %.2f",valorTotalDaCompra);
+            printf("\nCom 4 parcelas de R$ %.2f",parcelas,"\n");
+            printf("\nCOMPRA REALIZADA COM SUCESSO!!");
+            printf("\nOBRIGADO PELA PREFERENCIA!!\nVOLTE SEMPRE!!\n\n");
+
+		}else if (quantidadeDeParcelas <=5 & quantidadeDeParcelas>4) {
+
+            juros = 5.9/100;
+            valorTotalDaCompra = valorDaCompra * (1 + juros);
+            parcelas = valorTotalDaCompra/5;
+            printf("\nPerfeito! O valor total da compra sera de R$ %.2f",valorTotalDaCompra);
+            printf ("\nCom 5 parcelas de R$ %.2f",parcelas,"\n");
+            printf ("\nCOMPRA REALIZADA COM SUCESSO!!");
+            printf ("\nOBRIGADO PELA PREFERENCIA!!\nVOLTE SEMPRE!!\n\n");
+
+		}else{
+
+            printf("\n-----#OPCAO INVALIDA# ----\n\n");
+
+        }
+
+    }else if(respostaParcela>=0  & respostaParcela<1) {
+
+        printf ("\nOK! O valor da compra A VISTA e: R$ %.2f",valorDaCompra);
+        printf ("\nCOMPRA REALIZADA COM SUCESSO!!");
+        printf ("\nOBRIGADO PELA PREFERENCIA!!\nVOLTE SEMPRE!!\n\n");
+
+    }else {
+
+
+        printf("\n-----#OPCAO INVALIDA# ----\n\n");
+
     }
 
+    system("pause");
     return 0;
 }
